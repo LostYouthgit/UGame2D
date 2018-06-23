e78cae452a2dce61a8d3dd899c8c4c837c822452
LostYouthgit <you@example.com>
2018-06-23 18:49:34 +0300

    add

---
+++ Player2Beh.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Beh : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent <Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		rb.MovePosition (rb.position + Vector2.left * speed * Time.deltaTime);
+		//rb.Gravity = 10;
+	}
+}
