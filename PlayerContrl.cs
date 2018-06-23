323bb5a458a7ed7a462bdd0c353b2f74a8a9dabd
LostYouthgit <you@example.com>
2018-06-23 15:40:23 +0300

    new.cs

---
+++ PlayerContrl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerContrl : MonoBehaviour {
+
+	public float speed = 20f;
+	private Rigidbody2D rb;
+
+	private bool fall;
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent <Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float moveX = Input.GetAxis("Horizontal");
+		rb.MovePosition (rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+		rb.AddForce (Vector2.up + 8000);
+	}
+}
